82ce8a5d9a0d1b8c0db12454d9d69bc5121ac8c0
Tarun Konda <kondatarun353@gmail.com>
2026-01-07 17:02:38 +0530

    Add files via upload

---
+++ day1 py.py
@@ -0,0 +1,4 @@
+print("My self tarun")
+print("Day 1 python at codegnan")
+print("I am performing some output operations")
+
---
+++ day2.py
@@ -0,0 +1,16 @@
+#datatypes,variables and IO funtions
+name="Tarun" #string type
+age=71 #integer type
+weight=30.5 #float type
+list1=[1,2,5,1.2,"hi",(10,20)] #list of items
+tuple1=(1,4,1.2,"welcome") #tuple of items
+cards ={"A",2,3,4,5,6,7,8,9,"J","Q","K",2,3,5} #set datatype
+student_marks={"maths":90,"telugu":30}
+print("my name is :",name)
+print("my weight is =",weight)
+print("my age is =",age)
+print("list of numbers:",list1)
+print("tuple of items:",tuple1)
+print("pack of cards :",cards)
+print("student subject wise marks:",student_marks)
+        
