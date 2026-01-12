38fbba6145b742cc2ecdb1a36355b10da53291a9
Tarun Konda <kondatarun353@gmail.com>
2026-01-12 17:04:29 +0530

    Add files via upload

---
+++ day6 nested if,loops.py
@@ -0,0 +1,43 @@
+"""n=int(input())
+if n%2==0:
+    if n>-1:
+        print("number is even and positive")
+    else:
+        print("number is even and negative")
+else:
+        if n>-1:
+            print("number is odd and positive")
+        else:
+            print("number is odd and negative")"""
+        
+#loops
+#check which number is even and wnich number is odd
+
+"""lst=(1,2,3,4)
+for num in lst:
+    if num%2==0:
+        print(num,"-is even")
+    else:
+        print(num,"-is odd")
+print("program done")"""
+
+#check which number is posituve and wnich number is negative
+
+"""lst=(1,2,-1,-10,3,4)
+for num in lst:
+    if num>=0:
+        print(num,"-is positive")
+    else:
+        print(num,"-is negative")
+print("program done")"""
+
+#range
+#print numbers from 0 to 10 using range
+
+"""for num in range(10):
+    print(num)"""
+#print even numbers 0to10
+
+
+for num in range(0,10,2):
+    print(num)
